8e3edc8330bb3a73ab17ad821465d509a9b765fb
Brown <rgbrow@sandia.gov>
2024-02-13 12:58:34 -0700

    Rebased to main and refactored provider unit tests

--- scubagoggles/execute_unit_tests.py
+++ scubagoggles/execute_unit_tests.py
@@ -4,28 +4,31 @@ import unittest
 import sys
 import os
 from pathlib import Path
-from provider import get_spf_records, get_dkim_records, get_dmarc_records, get_super_admins, get_dnsinfo, get_ous, get_toplevel_ou, get_tenant_info, get_gws_logs, get_group_settings, call_gws_providers
-from auth import gws_auth
-from main import get_gws_args
-from orchestrator import gws_products, run_gws_providers, rego_eval, pluralize, run_reporter, run_cached, start_automation
+from scubagoggles.provider import Provider
+from scubagoggles.auth import gws_auth
+from scubagoggles.main import get_gws_args
+from scubagoggles.orchestrator import gws_products, run_gws_providers, rego_eval, pluralize, run_reporter, run_cached, start_automation, generate_summary
 from googleapiclient.discovery import build
 import argparse
 
 
-creds = gws_auth((Path.cwd() / "../credentials.json").resolve(), "amart24@scubagws.org")
-#creds = gws_auth("../credentials.json")
+#creds = gws_auth((Path.cwd() / "../credentials.json").resolve(), "amart24@scubagws.org")
+creds = gws_auth("../credentials.json")
 
 services = {}
 services['directory'] = build('admin', 'directory_v1', credentials=creds)
 services['reports'] = build('admin', 'reports_v1', credentials=creds)
 services['groups'] = build('groupssettings','v1', credentials=creds)
 
+#Create instance of provider to use for unit testing
+provider = Provider(services, 'C03ymv5su')
+
 class TestArgs:
     def __init__(self):
         self.scuba_cmd='gws'
-        self.baselines=['gmail']
+        self.baselines=['sites']
         self.outputpath='../'
-        self.credentials='.,/credentials.json'
+        self.credentials='../credentials.json'
         self.subjectemail=None
         self.customerid='my_customer'
         self.opapath='../'
@@ -44,6 +47,9 @@ class TestArgs:
 # class ProviderTests(unittest.TestCase):
 #     """Test cases for provider"""
 
+#     # def list_domains(self) -> list:
+#     #   print("test")
+
 #     # def test_get_spf_records(self):
 #     #     print("test")
     
@@ -57,38 +63,35 @@ class TestArgs:
 #     #     print("test")
 
 #     def test_get_super_admins_is_dict(self):
-#         gsa_out = get_super_admins(services["directory"], "C03ymv5su")
+#         gsa_out = provider.get_super_admins()
 #         self.assertIsInstance(gsa_out,dict)
 
 #     def test_get_super_admins_contains_list(self):
-#         gsa_out = get_super_admins(services["directory"], "C03ymv5su")
+#         gsa_out = provider.get_super_admins()
 #         self.assertIsInstance(gsa_out['super_admins'],list)
 
 #     def test_get_ous(self):
-#         ou_out = get_ous(services["directory"], "C03ymv5su")
+#         ou_out = provider.get_ous()
 #         self.assertIsInstance(ou_out,dict)
 
 #     def test_get_toplevel_ou(self):
-#         tlou_out = get_toplevel_ou(services["reports"], "C03ymv5su")
+#         tlou_out = provider.get_toplevel_ou()
 #         self.assertIsInstance(tlou_out,str)
 
 #     def test_get_tenant_info(self):
-#         ti_out = get_tenant_info(services["reports"], "C03ymv5su")
+#         ti_out = provider.get_tenant_info()
 #         self.assertIsInstance(ti_out,dict)
 
 #     def test_get_gws_logs(self):
-#         print("test")
-#         logs_out = get_gws_logs(gws_products()["gws_baselines"], services["reports"], "CHANGE_APPLICATION_SETTING")
+#         logs_out = provider.get_gws_logs(gws_products()["gws_baselines"],"CHANGE_APPLICATION_SETTING")
 #         self.assertIsInstance(logs_out,dict)
 
 #     def test_get_group_settings(self):
-#         print("test")
-#         gs_out = get_tenant_info(services["groups"], "C03ymv5su")
+#         gs_out = provider.get_tenant_info()
 #         self.assertIsInstance(gs_out,dict)
 
 #     def test_call_gws_providers(self):
-#         print("test")
-#         gp_out = call_gws_providers(gws_products()["gws_baselines"], services["groups"], True, "C03ymv5su")
+#         gp_out = provider.call_gws_providers(gws_products()["gws_baselines"], True)
 #         self.assertIsInstance(gp_out,dict)
 
 class OrchestratorTests(unittest.TestCase):
@@ -99,14 +102,11 @@ class OrchestratorTests(unittest.TestCase):
         self.assertIsInstance(prod_out,dict)
 
     #test takes too long to run for developing. Uncomment when unit tests are complete. Runs provider. 
-    # def test_run_gws_providers(self):
-    #     test_args = TestArgs()
-    #     failure = False
-    #     try:
-    #         run_gws_providers(test_args, services)
-    #     except:
-    #         failure = true
-    #     self.assertEqual(failure, False)
+    def test_run_gws_providers(self):
+        test_args = TestArgs()
+        run_gws_providers(test_args, services)
+        print(os.listdir('../'))
+        self.assertEqual(1,1)
 
     # def test_rego_eval(self):
     #     #rego_eval produces a test results json file 
