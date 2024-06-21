5d479de17ddd51cb02965e1a8d6bf950f6bd851f
Evangelista <jfevang@sandia.gov>
2024-06-20 22:29:21 -0700

    #200 fixing pylint error with f-string

--- scubagoggles/reporter/reporter.py
+++ scubagoggles/reporter/reporter.py
@@ -319,7 +319,7 @@ successful_calls : set, unsuccessful_calls : set) -> list:
                                 'Details': details})
         full_name = prod_to_fullname[product]
         markdownGroupName = "-".join(baseline_group['GroupName'].split())
-        groupReferenceURL = f'<a href="{SCUBA_GITHUB_URL}/tree/main/baselines/{full_name} Minimum Viable Secure Configuration Baseline v0.2.md#{baseline_group['GroupNumber']}-{markdownGroupName}" target="_blank">{baseline_group['GroupName']}</a>'
+        groupReferenceURL = f'<a href="{SCUBA_GITHUB_URL}/tree/main/baselines/{full_name} Minimum Viable Secure Configuration Baseline v0.2.md#{baseline_group["GroupNumber"]}-{markdownGroupName}" target="_blank">{baseline_group["GroupName"]}</a>'
         fragments.append(f"<h2>{product_upper}-{baseline_group['GroupNumber']} \
         {groupReferenceURL}</h2>")
         fragments.append(create_html_table(table_data))
