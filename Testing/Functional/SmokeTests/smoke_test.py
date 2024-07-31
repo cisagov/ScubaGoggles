4f8c951026d3b7a4bad32fb27eb6bbebd407755e
Mitchel Baker <mitchel.baker@cisa.dhs.gov>
2024-07-31 21:46:24 +0000

    cleanup workflows/actions; add check for valid json; add types

--- Testing/Functional/SmokeTests/smoke_test.py
+++ Testing/Functional/SmokeTests/smoke_test.py
@@ -10,37 +10,19 @@ import os
 from smoke_test_utils import verify_all_outputs_exist, verify_output_type
 
 class SmokeTest:
-    def test_venv_creation(self):
+    def test_scubagoggles_output(self, subjectemail):
         try:
-            result = subprocess.run(["ls", ".venv"], shell=True, capture_output=True, text=True, check=True)
-            if "Scripts" in result.stdout: 
-                assert True
-        except subprocess.CalledProcessError as e:
-            pytest.fail(f"An error occurred, {e}")
-
-    def test_scubagoggles(self, subjectemail):
-        try:
-            command = f"scubagoggles gws --subjectemail {subjectemail} --quiet"
+            command: str = f"scubagoggles gws --subjectemail {subjectemail} --quiet"
             subprocess.run(command, shell=True)
 
-            prefix = "GWSBaselineConformance"
-            directories = [d for d in os.listdir() if os.path.isdir(d) and d.startswith(prefix)]
+            prefix: str = "GWSBaselineConformance"
+            directories: list[str] = [d for d in os.listdir() if os.path.isdir(d) and d.startswith(prefix)]
             directories.sort(key=lambda d: os.path.getctime(d), reverse=True)
 
-            cwd = os.getcwd()
-            output_path = os.path.join(cwd, directories[0])
-            contents = verify_output_type(output_path, [])
+            # Access the latest output directory at the 0th index after sorting
+            cwd: str = os.getcwd()
+            output_path: str = os.path.join(cwd, directories[0])
+            contents: list[str] = verify_output_type(output_path, [])
             verify_all_outputs_exist(contents)
         except (OSError, ValueError, Exception) as e:
             pytest.fail(f"An error occurred, {e}")
-
-#def create_venv(env):
-#    result = subprocess.run(["python", "-m", "venv", env])
-#    if result.returncode == 0:
-#        raise RuntimeError(f"Failed to create virtual environment, {result.stderr}")
-#
-#def activate_venv(env):
-#    command = f"{env}\\Scripts\\activate"
-#    result = subprocess.run(command)
-#    if result.returncode == 0:
-#        raise RuntimeError(f"Failed to activate virtual environment, {result.stderr}")
--- Testing/Functional/SmokeTests/smoke_test_utils.py
+++ Testing/Functional/SmokeTests/smoke_test_utils.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+import json
 
 required_contents = [
     "BaselineReports.html", 
@@ -21,25 +22,29 @@ required_contents = [
     "triangle-exclamation-solid.svg"
 ]
 
-def verify_output_type(output_path, contents):
-    entries = os.listdir(output_path)
-
+def verify_output_type(output_path: str, contents: list[str]) -> list[str]:
+    entries: list[str] = os.listdir(output_path)
     for entry in entries:
         contents.append(entry)
-
         # Check if entry is a valid directory or file
-        child_path = os.path.join(output_path, entry)
+        child_path: str = os.path.join(output_path, entry)
         if os.path.isdir(child_path):
             assert True
             verify_output_type(child_path, contents)
         elif os.path.isfile(child_path):
+            # Check for valid json
+            if child_path.endswith(".json"):
+                try:
+                    with open(child_path) as jsonfile:
+                        json.load(jsonfile)
+                except ValueError as e:
+                    pytest.fail(f"Invalid json, ${e}")
             assert True
         else:
             raise OSError(f"Entry is not a directory or file (symlink, etc.)")
-
     return contents
 
-def verify_all_outputs_exist(contents):
+def verify_all_outputs_exist(contents: list[str]):
     for required_content in required_contents:
         if required_content in contents:
             assert True
