65a058236c670fc8001e0c5b160a406a6a25eed6
Mitchel Baker <mitchel.baker@cisa.dhs.gov>
2024-08-01 22:55:05 +0000

    expand selenium testing of scubagoggles reports, checks parent/individual reports

--- Testing/Functional/SmokeTests/smoke_test.py
+++ Testing/Functional/SmokeTests/smoke_test.py
@@ -18,6 +18,7 @@ from smoke_test_utils import (
     verify_all_outputs_exist, 
     verify_output_type,
 )
+from scubagoggles.orchestrator import gws_products
 
 class SmokeTest:
     def test_scubagoggles_output(self, subjectemail):
@@ -40,32 +41,29 @@ class SmokeTest:
             h1 = browser.find_element(By.TAG_NAME, "h1").text
             assert h1 == "SCuBA GWS Security Baseline Conformance Reports"
 
-            # verify domain is present in agency table
-            agency_table = browser.find_element(By.TAG_NAME, "table")
-            tbody = agency_table.find_element(By.TAG_NAME, "tbody")
-            rows = tbody.find_elements(By.TAG_NAME, "tr")
-            assert len(rows) == 2
-            assert rows[1].find_elements(By.TAG_NAME, "td")[0].text == domain
+            products = {
+                product: { "title": f"{product} Baseline Report" }
+                for product in gws_products()["prod_to_fullname"].values()
+            }
 
-            baseline_names = [
-                "Google Calendar", 
-                "Google Chat", 
-                "Google Classroom", 
-                "Common Controls", 
-                "Google Drive and Docs",
-                "Gmail",
-                "Groups for Business",
-                "Google Meet",
-                "Rules",
-                "Google Sites"
-            ]
+            print(products)
+
+            # Before entering loop check that we actually display 10 rows in table
 
             for i in range(10): 
+                # verify domain is present in agency table
+                tenant_table = browser.find_element(By.TAG_NAME, "table")
+                tbody = tenant_table.find_element(By.TAG_NAME, "tbody")
+                rows = tbody.find_elements(By.TAG_NAME, "tr")
+                assert len(rows) == 2
+                assert rows[1].find_elements(By.TAG_NAME, "td")[0].text == domain
+
                 reports_table = browser.find_elements(By.TAG_NAME, "table")[1]
                 tbody = reports_table.find_element(By.TAG_NAME, "tbody")
                 rows = tbody.find_elements(By.TAG_NAME, "tr")
                 td = rows[i].find_elements(By.TAG_NAME, "td")[0]
-                assert td.text in baseline_names
+                product = td.text
+                assert product in products
 
                 individual_report_anchor = td.find_element(By.TAG_NAME, "a")
                 href = individual_report_anchor.get_attribute("href")
@@ -73,19 +71,44 @@ class SmokeTest:
                 current_url = browser.current_url
                 assert href == current_url
 
+                # Check at the individual report level
+                tenant_table = browser.find_element(By.TAG_NAME, "table")
+                tbody = tenant_table.find_element(By.TAG_NAME, "tbody")
+                rows = tbody.find_elements(By.TAG_NAME, "tr")
+                assert len(rows) == 2
+                assert rows[1].find_elements(By.TAG_NAME, "td")[0].text == domain
+
+                h1 = browser.find_element(By.TAG_NAME, "h1").text
+                print(products[product])
+                print(products[product]["title"])
+                assert h1 == products[product]["title"]
+                
+                policy_tables = browser.find_elements(By.TAG_NAME, "table")
+                for table in policy_tables[1:]:
+                    # Verify policy table headers are correct 
+                    headers = (
+                        table.find_element(By.TAG_NAME, "thead")
+                        .find_elements(By.TAG_NAME, "tr")[0]
+                        .find_elements(By.TAG_NAME, "th")
+                    )
+                    assert len(headers) == 5
+                    assert headers[0].text == "Control ID"
+                    assert headers[1].text == "Requirement" or "Rule Name"
+                    assert headers[2].text == "Result"
+                    assert headers[3].text == "Criticality"
+                    assert headers[4].text == "Details" or "Rule Description"
+
+                    # Verify policy table rows are populated
+                    tbody = table.find_element(By.TAG_NAME, "tbody")
+                    rows = tbody.find_elements(By.TAG_NAME, "tr")
+                    assert len(rows) > 0
+
                 browser.back()
                 WebDriverWait(browser, 10).until(
                     expected_conditions.presence_of_element_located(
                         (By.TAG_NAME, "body")
                     )
                 )
-                
-            # Navigation to detailed reports 
-
-            # Check links work
-
-            # Verify tables are populated
-
-
         except Exception as e:
+            browser.quit()
             pytest.fail(f"An error occurred, {e}")
