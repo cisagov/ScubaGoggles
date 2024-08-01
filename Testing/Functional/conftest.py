fecba85f24b6d508764e15b25214e626707bd585
Mitchel Baker <mitchel.baker@cisa.dhs.gov>
2024-08-01 01:38:45 +0000

    added test_scubagoggles_report pytest with selenium; testing BaselineReports.html successfully so far

--- Testing/Functional/conftest.py
+++ Testing/Functional/conftest.py
@@ -1,5 +1,11 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 
 def pytest_addoption(parser):
     parser.addoption("--subjectemail", action="store")
@@ -15,6 +21,6 @@ def domain(pytestconfig):
 
 @pytest.fixture
 def browser():
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=chrome_options)
     yield driver
     driver.quit()
