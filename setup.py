9d36ffee115f88a08f7640f3220130c86c591a45
Annie ORourke (MU) <aorourke@monmouth.edu>
2024-08-29 09:38:55 -0400

    update dependencies versions

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ ScubaGoggles setuptools
 from setuptools import setup, find_packages
 
 setup(name='scubagoggles',
-      version='0.2.0',
+      version='0.3.0',
       description='SCuBA security baseline assessment tool',
       author='CISA',
       packages=find_packages(exclude=['__pycache__']),
@@ -12,16 +12,16 @@ setup(name='scubagoggles',
         'scubagoggles': ['reporter/**/*']
       },
       include_package_data=True,
-      python_requires='>=3.7.16',
+      python_requires='>=3.10.0',
       install_requires=[
-        'google-api-python-client==1.7.9',
-        'google-auth-httplib2==0.0.3',
-        'google-auth-oauthlib==0.4.0',
-        'MarkupSafe==2.1.1',
+        'google-api-python-client==2.142.0',
+        'google-auth-httplib2==0.2.0',
+        'google-auth-oauthlib==1.2.1',
+        'MarkupSafe==2.1.5',
         'dnspython==2.6.1',
-        'pandas==2.0.0',
-        'tqdm==4.66.3',
-        'requests==2.32.0'
+        'pandas==2.2.0',
+        'tqdm==4.66.5',
+        'requests==2.32.3'
       ],
       entry_points={
           'console_scripts': ['scubagoggles=scubagoggles.main:dive']
