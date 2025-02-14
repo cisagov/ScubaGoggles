614dca0e76011fa4a2db9489954730954c030d84
Roy Lane <rlane@mitre.org>
2025-02-14 10:42:27 -0500

    update version number from 0.4.0 to 0.4.1

--- scubagoggles/__init__.py
+++ scubagoggles/__init__.py
@@ -4,4 +4,4 @@ This file contains the OFFICIAL source of the ScubaGoggles version number.
 All other references to the version number are derived from this value.
 """
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
