c4f56249e02f72cc9ec9e56e74a7d98afa062e13
Devesh Agarwal <devesh.agarwal@ecstech.com>
2025-08-26 15:58:26 -0400

    updated opa installation for special case of windows and arm

--- scubagoggles/getopa.py
+++ scubagoggles/getopa.py
@@ -174,8 +174,9 @@ def opa_filespec(opa_dir: Path = None):
     os_type = platform.system().lower()
 
     arch = platform.machine().lower()
-
-    if arch == 'x86_64':
+    
+    # Special case for Windows os and Arm64 arch
+    if arch == 'x86_64' or os_type == 'windows':
         arch = 'amd64'
     elif arch.startswith('arm'):
         arch = 'arm64'
