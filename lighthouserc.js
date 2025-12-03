5ea23699e03f0ec0610900ac3f8b78e51592e744
obed ampah <obed.ampah@ecstech.com>
2025-12-03 13:41:14 -0800

    test - revert

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: './GWSBaselineConformance',
+      staticDistDir: './',
     },
     upload: {
       target: 'temporary-public-storage',
