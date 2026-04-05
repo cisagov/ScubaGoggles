0c7344c63ab23b926decb2bb2918a450c7707153
Paul Vanderveen <pvanderveen@mitre.org>
2026-04-04 22:50:18 -0400

    pylint fix

--- scubagoggles/main.py
+++ scubagoggles/main.py
@@ -224,7 +224,8 @@ def get_gws_args(parser: argparse.ArgumentParser, user_config: UserConfig):
                 'to retrieve any TXT records (i.e. required  by specific SCuBA policy).' +
                 'DoH queries will only be made if the traditional DNS queries fail' +
                 'Optional; If not provided, the DoH query will use a default ' +
-                'list of DoH servers: "cloudflare-dns.com", "2606:4700:4700::1111", "1.1.1.1". Example:' +
+                'list of DoH servers: "cloudflare-dns.com", "2606:4700:4700::1111", '
+                '"1.1.1.1". Example:' +
                 '--preferreddohservers 8.8.8.8 cloudfare-dns.com')
 
     parser.add_argument('--preferreddohservers',
