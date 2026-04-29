99f8b81f42ce5779a57c72070632a47bad2a3b3d
Annie ORourke <aorourke@mitre.org>
2026-04-29 15:10:11 -0400

    Refactor reporter.py and Python unit test failures (#1009)
    * fix pr962 not merge issues
    
    * fix pylint and unit_test
    
    * add _reporter_path required by unit test
    
    * assert
    
    * update unit test
    
    * update tester
    
    * corrections after rebase with main
    
    * fix pylint error
    
    ---------
    
    Co-authored-by: Roy Lane <rlane@mitre.org>

--- scubagoggles/Testing/Unit/Python/reporter/test_reporter.py
+++ scubagoggles/Testing/Unit/Python/reporter/test_reporter.py
@@ -138,7 +138,6 @@ class TestReporter:
 
         (tmp / "styles" / "FrontPageStyle.css").write_text("main {}\n footer {}", encoding="utf-8")
         (tmp / "styles" / "main.css").write_text(":root {}", encoding="utf-8")
-        (tmp / "scripts" / "main.js").write_text("const testVar = 0;", encoding="utf-8")
 
         # Patch the actual Reporter class attribute for _reporter_path,
         # otherwise the local instance returned by _reporter() won't use the temp files.
@@ -168,10 +167,9 @@ class TestReporter:
 
         assert "{{" not in html and "}}" not in html
         assert "<style>" in html and "</style>" in html
-        assert "main {}" in html and "footer {}" in html
-        assert ":root {}" in html
+        assert "main {" in html and "footer {" in html
+        assert ":root {" in html
         assert "<script>" in html and "</script>" in html
-        assert "const testVar = 0;" in html
 
         assert "sgr_settings" in html and "data-darkmode=\"true\"" in html
         assert "sgr_settings" in html and "data-redaction=\"true\"" in html
@@ -437,8 +435,6 @@ class TestReporter:
         )
 
         (tmp / "styles" / "main.css").write_text(":root {}", encoding="utf-8")
-        (tmp / "scripts" / "main.js").write_text("const testVar = 0;", encoding="utf-8")
-
         # Patch the actual Reporter class attribute for _reporter_path,
         # otherwise the local instance returned by _reporter() won't use the temp files.
         monkeypatch.setattr(Reporter, "_reporter_path", tmp, raising=True)
